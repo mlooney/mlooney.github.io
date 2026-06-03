1e5df7da1fa9948a165ca522687cdf2242253001
McClain Looney <mlooney@gmail.com>
2026-06-02 21:41:49 -0500

    wasm curse

---
+++ curse_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__convert__closures_____invoke__h61039d6f01dbb8cf: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h338ee61fb4aeb506: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0c2ca914beb92ac6_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__he22bc23644c4f594: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h30e29b433d3ee75c: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h04818a4a8c1a1405: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
